898191705583da61e56cc4f5c30aa466fa13f7e9
eddie <carpioe@hartwick.edu>
2019-11-14 08:50:29 -0500

    updated

--- human.py
+++ human.py
@@ -1,1 +1,13 @@
+class human():
+
+    def __init__(self,name,age,gender,profession):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.profession = profession
+
+    def description(self):
+        print(f"""My name is {self.name},my age is {self.age}I am a {self.gender}.
+                and my profession is {self.profession}""")
+
 
